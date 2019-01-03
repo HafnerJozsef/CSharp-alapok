75c26ab8124adca8be41ef64ade9deb1dc210048
HafnerJozsef <hafner1975@gmail.com>
2019-01-03 21:10:33 +0100

    #2: Síkidomok területének számítása

---
+++ 02Nap/02SikidomokTerulete/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _02SikidomokTerulete
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var square = new Square(side: 4);
+
+            Console.WriteLine($"A négyzet területe: {square.Area()}");
+
+            var circle = new Circle(radius: 5);
+            Console.WriteLine($"A kör területe: {circle.Area()}");
+
+            var triangle = new Triangle(trianglebase: 6, height: 4);
+            Console.WriteLine($"A háromszög területe: {triangle.Area()}");
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
