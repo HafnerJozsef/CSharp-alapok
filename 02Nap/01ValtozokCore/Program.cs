6c22a0935191344c553b777ba101a5db3423b977
HafnerJozsef <hafner1975@gmail.com>
2019-01-03 20:09:08 +0100

    #1: sokadik próba és javítás

---
+++ 02Nap/01ValtozokCore/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _01ValtozokCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
