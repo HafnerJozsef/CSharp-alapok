003db430dea17c624f714ec5ee520aa3723c50c6
HafnerJozsef <hafner1975@gmail.com>
2019-01-03 22:27:31 +0100

    #2: problémafelvetés (érmehamisítás)

---
+++ 02Nap/03Ermehamisitas/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _03Ermehamisitas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var coin = new Coin();
+            Console.WriteLine($"Az érmefeldobás eredménye: ");
+            for (int i = 0; i < 100; i++)
+            {
+                Console.Write($"{coin.Collect()},");
+            }
+            Console.ReadLine();           
+        }
+    }
+}
