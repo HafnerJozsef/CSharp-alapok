be009b64031132df7ba55d8d2e1b2fe679ee6b44
HafnerJozsef <hafner1975@gmail.com>
2019-01-03 21:35:09 +0100

    #2: területek összeadása

--- 02Nap/02SikidomokTerulete/Program.cs
+++ 02Nap/02SikidomokTerulete/Program.cs
@@ -16,9 +16,16 @@ namespace _02SikidomokTerulete
             var triangle = new Triangle(trianglebase: 6, height: 4);
             Console.WriteLine($"A háromszög területe: {triangle.Area()}");
 
+            //területek összeadása:
+            var areasum = square.Area();
+            areasum = areasum + circle.Area();
+            //előző művelet rövidítve
+            areasum += triangle.Area();
+
+            //profibb munka (mi van, ha több száz síkidom területét kell összeadni):
 
 
-
+            Console.WriteLine($"A területek összege: {areasum}");
 
             Console.ReadLine();
         }
