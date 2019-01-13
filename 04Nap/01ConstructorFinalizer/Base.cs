47a20cd7ba4462ad732d8f6edf9356474c436385
HafnerJozsef <hafner1975@gmail.com>
2019-01-13 10:29:03 +0100

    #2: constructor, leszármazás

---
+++ 04Nap/01ConstructorFinalizer/Base.cs
@@ -0,0 +1,11 @@
+﻿namespace _01ConstructorFinalizer
+{
+    public class Base
+    {
+        //ez egy különleges fgv : ez a constructor
+        public Base()
+        {
+            System.Console.WriteLine("Base létrehozó: Base()");
+        }
+    }
+}
