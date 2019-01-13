649d6eb6332a6b17c248b37f30e24af7712f2046
HafnerJozsef <hafner1975@gmail.com>
2019-01-13 11:54:49 +0100

    #2: konstruktorok egymáshoz való viszonyai

--- 04Nap/01ConstructorFinalizer/Program.cs
+++ 04Nap/01ConstructorFinalizer/Program.cs
@@ -9,16 +9,25 @@ namespace _01ConstructorFinalizer
             var b = new Base();
             Console.WriteLine();
 
-            var m = new Middle();
-            Console.WriteLine();
+            //var m = new Middle();
+            //Console.WriteLine();
 
-            var t = new Third();
-            Console.WriteLine();
+            //var t = new Third();
+            //Console.WriteLine();
 
             b = new Base("ez az alaposztály");
             Console.WriteLine();
 
             b = new Base("ez az alaposztály", "alap@alap.hu");
+            Console.WriteLine();
+
+            var m = new Middle("az a middle", "middlea@middle.hu");
+            Console.WriteLine($"név: {m.Name}, email: {m.Email}");
+            Console.WriteLine();
+
+
+            var t = new Third("az a third", "third@third.hu");
+            Console.WriteLine($"név: {t.Name}, email: {t.Email}");
             Console.WriteLine();
 
             Console.ReadLine();
