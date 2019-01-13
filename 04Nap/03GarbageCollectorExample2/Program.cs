42f8739ae1d618b634315cc8f659b18d19a2a142
HafnerJozsef <hafner1975@gmail.com>
2019-01-13 20:20:43 +0100

    #2: példakód a memóriakezeléshez

---
+++ 04Nap/03GarbageCollectorExample2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+
+namespace _03GarbageCollectorExample2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (!Console.KeyAvailable)
+            {
+                //var bitmap = new Bitmap(1280, 1024);
+                using (var bitmap = new Bitmap(2048, 2048)) { }
+            }
+        }
+    }
+}
