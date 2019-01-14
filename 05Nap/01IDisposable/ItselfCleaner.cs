1086c9f9f4d91c77456d53f2a618781f29a5b218
HafnerJozsef <hafner1975@gmail.com>
2019-01-14 16:42:39 +0100

    #4: IDisposable implementáció vázlat, nem tökéletes

---
+++ 05Nap/01IDisposable/ItselfCleaner.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace _01IDisposable
+{
+    public class ItselfCleaner : IDisposable
+    {
+        private bool isDisposed = false;
+        //első példa: menedzselt stream, de IDisposable felületet implementál
+        private Stream fileStream = new FileStream("file.txt",FileMode.Create);
+        //második példa : menedzselt lista, de nagy méretű
+        private List<string> managedMemory = new List<string>();
+        //harmadik példa: nem menedzselt memória
+        private IntPtr unmanagedMemory = IntPtr.Zero;
+
+        
+
+
+        public ItselfCleaner()
+        {
+            //menedzselt memória feltöltése
+            for (int i = 0; i < 1000000; i++)
+            {
+                managedMemory.Add("AAAAAAA");
+            }
+            //nem menedzselt memória lefoglalása
+            unmanagedMemory = Marshal.AllocHGlobal(1000000);
+            GC.AddMemoryPressure(1000000);
+        }
+
+        public void Dispose()
+        {
+            
+            if(isDisposed)
+            {
+                return;
+            }
+
+            //takarítunk
+
+            fileStream.Dispose();
+            fileStream = null;
+
+            managedMemory.Clear();
+            managedMemory = null;
+
+            Marshal.FreeHGlobal(unmanagedMemory);
+            GC.RemoveMemoryPressure(1000000);
+
+            isDisposed = true;
+        }
+
+        ~ItselfCleaner()
+        {
+            Dispose();
+        }
+    }
+}
---
+++ 05Nap/01IDisposable/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _01IDisposable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //készítünk egy önmaga után eltakarító osztályt
+            //var takarito = new ItselfCleaner();
+
+            //try
+            //{
+            //}     
+            //finally
+            //{
+            //    ((IDisposable)takarito).Dispose();
+            //}
+
+            using (var takarito = new ItselfCleaner())
+            {//ebben a kódblokkban van a  védendő használat
+
+            }
+        }
+    }
+}
