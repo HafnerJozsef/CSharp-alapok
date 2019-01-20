b0b95121634a72ad79bed63fc5ad2068c74716ec
HafnerJozsef <hafner1975@gmail.com>
2019-01-20 21:12:40 +0100

    #6: Kivételek teljesítményének a mérése

---
+++ 07Nap/05ExceptionPerformanceDotNetFramework/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05ExceptionPerformanceDotNetFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+
+
+
+            for (int i = 0; i < 1000; i++)
+            {
+                try
+                {
+                    throw new Exception();
+                }
+                catch (Exception)
+                { }
+            }
+
+            Console.WriteLine($"Eltelt idő: {sw.ElapsedTicks}");
+
+
+            sw.Restart();
+            for (int i = 0; i < 1000; i++)
+            {
+                //try
+                //{
+                //    throw new Exception();
+                //}
+                //catch (Exception)
+                //{ }
+            }
+
+            sw.Stop();
+            Console.WriteLine($"Eltelt idő: {sw.ElapsedTicks}");
+
+
+            Console.ReadLine();
+        }
+    }
+}
